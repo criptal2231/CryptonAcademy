128548defbf01008e758fc49439b79b845a7f705
criptal2231 <102367580+criptal2231@users.noreply.github.com>
2022-03-25 14:31:21 +0100

    Create Index.js

---
+++ показатель/Index.js
@@ -0,0 +1,7 @@
+echo "# CryptonAcademy" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin https://github.com/criptal2231/CryptonAcademy.git
+ git push - ты главный
